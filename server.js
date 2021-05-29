8791629a20fd9113219d4f3f0e86e7859005865a
ryan-rozario <ryan.rozario1999@gmail.com>
2021-05-29 16:48:41 +0400

    shuffling cards and assigning roles

--- server.js
+++ server.js
@@ -15,3 +15,36 @@ app.get('/', (req, res) => {
 
 app.use('/client', express.static(__dirname + '/client'));
 
+//TODO: make this a mapping for rooms later
+let numUsers = 0;
+
+const ROLES_INDIVIDUAL = ["Forensic Scientist", "Murderer", "Accomplice", "Witness"]
+const ROLE_NORMAL = "Investigator"
+
+var gameState = 0
+
+function shuffleFisherYates(array) {
+    let i = array.length;
+
+    if (n > i)
+        throw new RangeError("getRandom: more elements taken than available");
+
+
+    while (i--) {
+      const ri = Math.floor(Math.random() * (i + 1));
+      [array[i], array[ri]] = [array[ri], array[i]];
+    }
+    return array;
+}
+
+function assignRoles(array) {
+    //shuffleFisherYates
+    array = shuffleFisherYates(array);
+    while (n--) {
+        array[n].role = ROLES_INDIVIDUAL[n];
+    }
+    return array;
+}
+
+
+
